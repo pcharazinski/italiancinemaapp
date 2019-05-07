e9880add735c5738dc3bd92cb63f8ca9fb26a370
twomeaningsof <twomeaningsof@gmail.com>
2019-05-07 20:14:33 +0200

    pizza movies

---
+++ movies.js
@@ -0,0 +1,64 @@
+const movie1 = {
+
+    "Title": "Pizza, Beer, and Cigarettes",
+    "Year": "1998",
+    "Runtime": "92 min",
+    "Genre": "Crime, Drama",
+    "Director": "Israel Adrián Caetano, Bruno Stagnaro",
+    "Actors": "Héctor Anglada, Jorge Sesán, Pamela Jordán, Adrián Yospe",
+    "Plot": "A couple of friends work for a taxi driver to rob his passengers, but they feel like they're getting ripped off. They decide to plan their own robberies, but they are amateurs and things ...",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BY2E3YTVkYzUtNTljOC00NDg2LTlkNmQtYzJkMjAyOGFjMzI1XkEyXkFqcGdeQXVyMzU1ODUxOTk@._V1_SX300.jpg"
+
+}
+
+const movie2 = {
+
+    "Title": "Fat Pizza",
+    "Year": "2003",
+    "Runtime": "96 min",
+    "Genre": "Action, Adventure, Comedy, Crime, Thriller",
+    "Director": "Paul Fenech",
+    "Actors": "Paul Fenech, Tahir Bilgic, Rob Shehadie, Bill Bentley",
+    "Plot": "The life at a suburban Sydney pizzeria.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTFhMzE2NzgtYWJlYS00NWVkLWJkYTgtMTliZDZjNjg4MmMzXkEyXkFqcGdeQXVyMjMyNTkxNzY@._V1_SX300.jpg"
+
+}
+
+const movie3 = {
+
+    "Title": "Pizza",
+    "Year": "2012",
+    "Runtime": "127 min",
+    "Genre": "Horror, Thriller",
+    "Director": "Karthik Subbaraj",
+    "Actors": "Vijay Sethupathi, Ramya Nambeeshan, Aadukalam Naren, Karunakaran",
+    "Plot": "A pizza delivery boy lands in a mysterious circumstance and it works a dramatic change in his life.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNDc3MTM0ZDItZjcwMy00MmI5LTlhMTctYmQ4NjM1MDM5MTRlXkEyXkFqcGdeQXVyODk1MzE5NDA@._V1_SX300.jpg"
+
+}
+
+const movie4 = {
+
+    "Title": "Two Guys, a Girl and a Pizza Place",
+    "Year": "1998–2001",
+    "Runtime": "30 min",
+    "Genre": "Comedy, Romance",
+    "Director": "N/A",
+    "Actors": "Traylor Howard, Ryan Reynolds, Richard Ruccolo, Suzanne Cryer",
+    "Plot": "Three twenty-somethings share a Boston apartment and hang around (and work) at a pizza place.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BN2I3NWRlYzYtMDNkZS00NmJlLWIyYzctYzkwODM3YWRiZjk1XkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_SX300.jpg"
+
+}
+
+const movie5 = {
+
+    "Title": "Mystic Pizza",
+    "Year": "1988",
+    "Runtime": "104 min",
+    "Genre": "Comedy, Drama, Romance",
+    "Director": "Donald Petrie",
+    "Actors": "Annabeth Gish, Julia Roberts, Lili Taylor, Vincent D'Onofrio",
+    "Plot": "Three teenage girls come of age while working at a pizza parlor in the Connecticut town of Mystic.",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMTU1NDk1MTkzMF5BMl5BanBnXkFtZTcwMzY0MDQ1NA@@._V1_SX300.jpg"
+
+}
